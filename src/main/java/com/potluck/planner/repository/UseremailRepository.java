ba07678643bdd87ea128713e2ac4aa88157b7741
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-10-20 21:17:52 -0400

    Add files via upload

---
+++ src/main/java/com/potluck/planner/repository/UseremailRepository.java
@@ -0,0 +1,12 @@
+package com.potluck.planner.repository;
+
+import com.potluck.planner.models.Useremail;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * The CRUD Repository connecting Useremail to the rest of the application
+ */
+public interface UseremailRepository
+        extends CrudRepository<Useremail, Long>
+{
+}
